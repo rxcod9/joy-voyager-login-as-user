4325ef675982a4e5934388b69c8051d4ae75925a
Ramakant <gangwar.ramakant@gmail.com>
2022-04-04 00:43:12 +0530

    Fix Action

--- src/Actions/LoginAsUserAction.php
+++ src/Actions/LoginAsUserAction.php
@@ -16,7 +16,7 @@ class LoginAsUserAction extends AbstractAction
 
     public function getIcon()
     {
-        return 'voyager-login-as-user';
+        return 'voyager-login-as-user fa-solid fa-user-secret';
     }
 
     public function getPolicy()
@@ -49,25 +49,6 @@ class LoginAsUserAction extends AbstractAction
                 $this->dataType->slug,
                 config('joy-voyager-login-as-user.not_allowed_slugs', [])
             );
-    }
-
-    public function massAction($ids, $comingFrom)
-    {
-        // GET THE SLUG, ex. 'posts', 'pages', etc.
-        $slug = $this->getSlug(request());
-
-        // GET THE DataType based on the slug
-        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
-
-        // Check permission
-        Gate::authorize('browse', app($dataType->model_name));
-
-        // Your macgic here
-
-        return redirect()->back()->with([
-            'message'    => __('joy-voyager-login-as-user::generic.successfully_login_as_usered') . " {$dataType->getTranslatedAttribute('display_name_singular')}",
-            'alert-type' => 'success',
-        ]);
     }
 
     protected function getSlug(Request $request)
